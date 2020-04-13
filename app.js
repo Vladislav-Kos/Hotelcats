621852766982e946afb27f9383e235610a3069d3
Vladislav-Kos <61909342+Vladislav-Kos@users.noreply.github.com>
2020-04-13 20:40:39 +0500

    3 (slick)

---
+++ app.js
@@ -0,0 +1,12 @@
+$('.slick-container').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+});
+$('.slider-container').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  arrows: true,
+});
