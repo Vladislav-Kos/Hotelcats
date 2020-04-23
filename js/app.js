6c79e92aae5a31c85d951567e84640edd4b7cbbe
Vladislav-Kos <61909342+Vladislav-Kos@users.noreply.github.com>
2020-04-23 21:04:58 +0500

    18

---
+++ js/app.js
@@ -0,0 +1,38 @@
+$(function() {
+    console.log("WORK")
+
+
+    $('.cat-menu').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+        dots: true,
+    });
+
+
+
+    $('.reviews-1').slick({
+                slidesToShow: 3,
+                slidesToScroll: 3,
+        arrows: true,
+        dots: true,
+                responsive: [
+                    {
+                        breakpoint: 992,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 2,
+                        }
+                    },
+                    {
+                        breakpoint: 450,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                        }
+                    }
+                ]
+            });
+
+});
